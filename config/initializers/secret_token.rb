d88db748fffc71ecf8115f17c2d9e1110021fdc6
Mihai Frisan <mihai@frisan.ro>
2014-04-23 13:05:03 +0300

    add application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoudLanding::Application.config.secret_key_base = '5bd208cbc0fe0b6ce3e4f8d9b760374e658b87a7d09299af67bc2cc4462fda9d7b130a4541f6f989de14faea8c05260f4e2fb56d43f8ed7c31277f9cd4460a30'

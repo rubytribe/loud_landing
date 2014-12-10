dd5d2aa9d2ec4b1ade1677655c4f1389fa116062
Adrian Prisacaru <adi_prisacaru@yahoo.com>
2014-12-10 16:37:27 +0200

    basic posts page

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,24 @@
+var picturePosition = 0;
+
+var increasePicturePosition = function(){
+  picturePosition += 1;
+  if (picturePosition == $('.posts .image-box').length) {
+    picturePosition = 0;
+  }
+}
+
+var showFirstPicture = function(){
+  $('.posts .image-box').eq(0).removeClass('hide');
+}
+
+var nextPicture = function(){
+  $('.posts .image-box').eq(picturePosition).addClass('hide');
+  increasePicturePosition();
+  $('.posts .image-box').eq(picturePosition).removeClass('hide');
+}
+
+
+$(document).ready(function(){
+  showFirstPicture();
+  setInterval(nextPicture, 5000);
+})

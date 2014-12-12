9ec143f14ed368699805e72268ca95733fc1baf2
Adrian Prisacaru <adi_prisacaru@yahoo.com>
2014-12-13 01:50:05 +0200

    posts page design

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -9,16 +9,55 @@ var increasePicturePosition = function(){
 
 var showFirstPicture = function(){
   $('.posts .image-box').eq(0).removeClass('hide');
+  // wait for the image to load
+  //while ($('.current-image-box .post-image').width() == 0) {
+  //  
+  //}
+  setHeaderWidth();
+  setBackground();
+}
+
+var getRandomPicture = function(){
+  var rand = Math.floor(Math.random() * $('.seen .image-box').length);
+  $('.posts').append($('.seen .image-box').eq(rand));
+  nextPicture();
 }
 
 var nextPicture = function(){
-  $('.posts .image-box').eq(picturePosition).addClass('hide');
-  increasePicturePosition();
-  $('.posts .image-box').eq(picturePosition).removeClass('hide');
+  $('.posts .image-box').eq(0).removeClass('hide');
+  setHeaderWidth();
+  setBackground();
+}
+
+var changePicture = function(){
+  // check if there is a current image
+  if ($('.posts .image-box').length > 0) {
+    $('.seen').append($('.posts .image-box').eq(0));
+    //$('.posts .image-box'.length > 0).eq(0).remove();
+    // check if there is a next image
+    if ($('.posts .image-box').length > 0) {
+      nextPicture();
+    }
+    else {
+      getRandomPicture();
+    }
+  }
+
+}
+
+var setHeaderWidth = function(){
+    $('.posts .image-box .user').eq(0).width($('.posts .image-box .image-wrapper').eq(0).width());
+}
+
+var setBackground = function(){
+  $('.background-image').css('background-image', "url(" + $('.posts .image-box .post-image').eq(0).attr('src') +")")
 }
 
 
-$(document).ready(function(){
-  showFirstPicture();
-  setInterval(nextPicture, 20000);
-})
+// wait for first picture to load
+setTimeout(function(){
+  $(document).ready(function(){
+    showFirstPicture();
+    setInterval(changePicture, 3000);
+  })
+}, 1000)
